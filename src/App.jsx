879103f0fd2f3b02b2a3848a6e6f417ab97b1a0a
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-08 13:05:30 +0100

    added routes for searching movie and render movie info

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Home from "./pages/Home/Home.jsx";
 import {Route, Routes} from "react-router-dom";
 import SharedLayout from "./components/common/SharedLayout/SharedLayout.jsx";
-import MoviesDetails from "./pages/MoviesDetails/MoviesDetails.jsx";
+import MovieDetails from "./pages/MovieDetails/MovieDetails.jsx";
 import Movies from "./pages/Movies/Movies.jsx";
 
 function App() {
@@ -10,6 +10,8 @@ function App() {
 		<Routes>
 			<Route path="/" element={<SharedLayout/>}>
 				<Route index element={<Home/>}/>
+				<Route path="/movies" element={<Movies/>}/>
+				<Route path="/movies/:movieId" element={<MovieDetails/>}/>
 			</Route>
 		</Routes>
 	)
