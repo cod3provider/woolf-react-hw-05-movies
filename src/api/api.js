58c98cdc9458fac14bd1a302331a038acafc5d51
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-08 11:46:28 +0100

    added function for searching movies

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_KEY = '86b2774f72e9923bd0f07dfe6f4fea8a';
+export const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 axios.defaults.params = {
@@ -10,6 +11,12 @@ axios.defaults.params = {
 
 export const getMovies = async () => {
   const response = await axios.get('trending/movie/week');
+  console.log(response.data.results);
+  return response.data.results;
+}
+
+export const searchMovies = async (query) => {
+  const response = await axios(`/search/movie?query=${query}`);
   console.log(response.data.results);
   return response.data.results;
 }
