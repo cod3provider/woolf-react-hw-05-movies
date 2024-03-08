cc13f8ac1379cb06db8a74c5589f3010599e9d6f
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-08 13:27:23 +0100

    refactored AboutMovie

--- src/components/AboutMovie/AboutMovie.jsx
+++ src/components/AboutMovie/AboutMovie.jsx
@@ -1,16 +1,16 @@
 import {POSTER_URL} from "../../api/api.js";
 
-const AboutMovie = ({ about }) => {
-	console.log(about)
+const AboutMovie = ({about}) => {
+	const { poster_path, title, vote_average, genres, overview } = about;
 
-	// const
 	return (
 		<div>
-			<img src={about.poster_path ? `${POSTER_URL}/${about.poster_path}` : <p>Film has no image</p>} alt='poster image'/>
-			<h1>{about.title}</h1>
-			<p>Score: {about.vote_average}</p>
-			<p>Genres: {about.genres.map(genre => genre.name).join(', ')}</p>
-			<p>Overview: {about.overview}</p>
+			<img src={poster_path ? `${POSTER_URL}/${poster_path}` : <p>Film has no image</p>}
+			     alt='poster image'/>
+			<h1>{title}</h1>
+			<p>Score: {(vote_average).toFixed(2)}</p>
+			<p>Genres: {genres.map(genre => genre.name).join(', ')}</p>
+			<p>Overview: {overview}</p>
 		</div>
 	)
 }
