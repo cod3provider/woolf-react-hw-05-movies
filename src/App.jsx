8a7798c558c13a2f629c6eb2933bc3c71555167b
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-08 17:10:01 +0100

    added route for cast

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {Route, Routes} from "react-router-dom";
 import SharedLayout from "./components/common/SharedLayout/SharedLayout.jsx";
 import MovieDetails from "./pages/MovieDetails/MovieDetails.jsx";
 import Movies from "./pages/Movies/Movies.jsx";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import Cast from "./components/Cast/Cast.jsx";
 
 function App() {
 	return (
@@ -11,7 +13,10 @@ function App() {
 			<Route path="/" element={<SharedLayout/>}>
 				<Route index element={<Home/>}/>
 				<Route path="/movies" element={<Movies/>}/>
-				<Route path="/movies/:movieId" element={<MovieDetails/>}/>
+				<Route path="/movies/:movieId" element={<MovieDetails/>}>
+					<Route path="cast" element={<Cast />}/>
+				</Route>
+				<Route path="*" element={<NotFound />}/>
 			</Route>
 		</Routes>
 	)
--- src/components/AboutMovie/AboutMovie.jsx
+++ src/components/AboutMovie/AboutMovie.jsx
@@ -1,12 +1,11 @@
 import {POSTER_URL} from "../../api/api.js";
 
 const AboutMovie = ({about}) => {
-	const { poster_path, title, vote_average, genres, overview } = about;
+	const {poster_path, title, vote_average, genres, overview} = about;
 
 	return (
 		<div>
-			<img src={poster_path ? `${POSTER_URL}/${poster_path}` : <p>Film has no image</p>}
-			     alt='poster image'/>
+			<img src={poster_path ? `${POSTER_URL}/${poster_path}` : <p>Film has no image</p>} alt='poster image'/>
 			<h1>{title}</h1>
 			<p>Score: {(vote_average).toFixed(2)}</p>
 			<p>Genres: {genres.map(genre => genre.name).join(', ')}</p>
