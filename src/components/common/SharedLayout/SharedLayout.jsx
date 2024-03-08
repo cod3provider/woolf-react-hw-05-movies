a7ca923e2ffcbffbc2f19cdb3d435c832b2a6e3f
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-08 12:24:46 +0100

    added styles to nav

--- src/components/common/SharedLayout/SharedLayout.jsx
+++ src/components/common/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,17 @@
 import {NavLink, Outlet} from "react-router-dom";
 import {Suspense} from "react";
+
 import Container from "../Container/Container.jsx";
+
+import s from './SharedLayout.module.css';
 
 const SharedLayout = () => {
 	return (
 		<Container>
 			<header>
-				<nav>
+				<nav className={s.nav}>
 					<NavLink to="/">Home</NavLink>
-					<NavLink to="/movies">Search Movies</NavLink>
+					<NavLink to="/movies">Search</NavLink>
 				</nav>
 			</header>
 			<Suspense fallback={<p>...Loading</p>}>
