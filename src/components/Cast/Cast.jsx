cc1f25a42caac9d80b624ea36830bdd2a1ecde67
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-08 17:08:43 +0100

    added Cast component

---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,43 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
+import {getCast, POSTER_URL} from "../../api/api.js";
+
+const Cast = () => {
+	const [cast, setCast] = useState([]);
+	const {movieId} = useParams();
+
+	useEffect(() => {
+		const getMovieCast = async () => {
+			try {
+				const data = await getCast(movieId);
+				console.log('data', data)
+				setCast(data);
+			}
+			catch (err) {
+				console.log(err);
+			}
+		}
+
+		getMovieCast();
+	}, [movieId]);
+
+	console.log('cast', cast);
+
+	const castItem = cast.map(({id, name, profile_path}) => (
+		<li key={id}>
+			<img src={`${POSTER_URL}/${profile_path}`} alt="actors photo" width='200'/>
+			<p>
+				{name}
+			</p>
+		</li>
+	))
+
+	return (
+		<ul>
+			{castItem}
+		</ul>
+	)
+}
+
+export default Cast;
