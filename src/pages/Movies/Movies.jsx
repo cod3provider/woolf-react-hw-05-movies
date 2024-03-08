47cdc2a524fd7e698cace5a981531096133ca5c7
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-08 11:44:46 +0100

    added Movies with form for searching

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react";
+import {useSearchParams} from "react-router-dom";
+import {searchMovies} from "../../api/api.js";
+
+const Movies = () => {
+	const [movies, setMovies] = useState([]);
+	const [query, setQuery] = useState('');
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const movieSearchParams = searchParams.get('query');
+
+	const handleChange = e => {
+		const {value} = e.target;
+		setQuery(value.toLowerCase());
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		setSearchParams({query: query});
+		setQuery('');
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<input
+					onChange={handleChange}
+					type="text"
+					value={query}
+					placeholder="Enter a name of film"
+				/>
+				<button type="submit">Search</button>
+			</form>
+
+		</>
+	)
+}
+
+export default Movies;
