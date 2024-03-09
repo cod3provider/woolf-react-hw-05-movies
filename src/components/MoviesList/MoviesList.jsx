ae3a8944f6fc6e6e4d337799a6527e81d14f6ee7
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-09 22:32:49 +0100

    added styles for MoviesList

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,11 +1,17 @@
 import MoviesItem from "./MoviesItem/MoviesItem.jsx";
 
+import s from './MoviesList.module.css';
 
 const MoviesList = ({movies}) => {
 	return (
-		<ul>
+		<ul className={s.list}>
 			{movies.map(({id, original_title, poster_path}) =>
-				<MoviesItem key={id} id={id} title={original_title} poster={poster_path}/>
+				<MoviesItem
+					key={id}
+					id={id}
+					title={original_title}
+					poster={poster_path}
+				/>
 			)}
 		</ul>
 
