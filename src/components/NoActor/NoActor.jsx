12db38cd382478140b6f2993062aa2dac0995b8a
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-09 22:33:01 +0100

    added component NoActor

---
+++ src/components/NoActor/NoActor.jsx
@@ -0,0 +1,11 @@
+import image from '../../assets/NoUser.png'
+
+const NoActor = () => {
+	return (
+		<>
+			<img src={image} alt="picture no image" width='200'/>
+		</>
+	)
+}
+
+export default NoActor;
