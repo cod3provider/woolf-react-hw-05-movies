94b97e2a78093d105f46eae97e66eeae91fb3b69
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-09 22:33:38 +0100

    added styles to SharedLayout

--- src/components/common/SharedLayout/SharedLayout.jsx
+++ src/components/common/SharedLayout/SharedLayout.jsx
@@ -4,14 +4,18 @@ import {Suspense} from "react";
 import Container from "../Container/Container.jsx";
 
 import s from './SharedLayout.module.css';
+import Logo from "../Logo/Logo.jsx";
 
 const SharedLayout = () => {
 	return (
 		<Container>
-			<header>
+			<header className={s.header}>
 				<nav className={s.nav}>
-					<NavLink to="/">Home</NavLink>
-					<NavLink to="/movies">Search</NavLink>
+					<Logo />
+					<div className={s.wrapper}>
+					<NavLink className={s.link} to="/">Home</NavLink>
+					<NavLink className={s.link} to="/movies">Search</NavLink>
+					</div>
 				</nav>
 			</header>
 			<Suspense fallback={<p>...Loading</p>}>
