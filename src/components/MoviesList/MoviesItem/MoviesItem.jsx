6ac1f6e251671844502e9b1d647aa920440e14b0
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-09 22:34:29 +0100

    added placeholder for films without posters, added styles to MoviesItem

--- src/components/MoviesList/MoviesItem/MoviesItem.jsx
+++ src/components/MoviesList/MoviesItem/MoviesItem.jsx
@@ -1,14 +1,34 @@
 import {Link, useLocation} from "react-router-dom";
+
 import {POSTER_URL} from "../../../api/api.js";
+
+import noImage from '../../../assets/No-Image-Movie.png'
+
+import s from '../MoviesList.module.css';
 
 const MoviesItem = ({id, title, poster}) => {
 	const location = useLocation();
+
 	return (
-		<li>
+		<li className={s.card}>
 			<Link to={`/movies/${id}`} state={{from: location}}>
-				<img src={poster ? `${POSTER_URL}${poster}` : <p>no image</p>} alt={`poster of film: ${title}`}/>
+				{
+					poster
+						?
+						<img
+							className={s.img}
+							src={`${POSTER_URL}${poster}`}
+							alt={`poster of film: ${title}`}
+						/>
+						:
+						<img
+							className={s.img}
+							src={noImage}
+							alt='no-image placeholder'
+						/>
+				}
+				<p className={s.title}>{title}</p>
 			</Link>
-			<p>{title}</p>
 		</li>
 	)
 }
