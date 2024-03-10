c231b881088bb32b51777f6c70ff22e5b9f2ffad
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-10 15:24:18 +0100

    added styles to AboutMovie

--- src/components/AboutMovie/AboutMovie.jsx
+++ src/components/AboutMovie/AboutMovie.jsx
@@ -1,15 +1,17 @@
 import {POSTER_URL} from "../../api/api.js";
 
+import s from './AboutMovie.module.css';
+
 const AboutMovie = ({about}) => {
 	const {poster_path, title, vote_average, genres, overview} = about;
 
 	return (
-		<div>
-			<img src={poster_path ? `${POSTER_URL}/${poster_path}` : <p>Film has no image</p>} alt='poster image'/>
+		<div className={s.wrap}>
+			<img className={s.img} src={poster_path ? `${POSTER_URL}/${poster_path}` : <p>Film has no image</p>} alt='poster image'/>
 			<h1>{title}</h1>
 			<p>Score: {(vote_average).toFixed(2)}</p>
 			<p>Genres: {genres.map(genre => genre.name).join(', ')}</p>
-			<p>Overview: {overview}</p>
+			<p className={s.overview}>Overview: {overview}</p>
 		</div>
 	)
 }
