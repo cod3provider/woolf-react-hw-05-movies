4e7368f305d5d07deae261a098f23d60cc481a1d
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-11 08:50:22 +0100

    small fixes of Movies component

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,67 +1,68 @@
-import {useEffect, useState} from "react";
-import {useSearchParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 import MoviesList from "../../components/MoviesList/MoviesList.jsx";
 
-import {searchMovies} from "../../api/api.js";
+import { searchMovies } from "../../api/api.js";
 
-import s from './Movies.module.css';
+import s from "./Movies.module.css";
 
 const Movies = () => {
-	const [films, setFilms] = useState([]);
-	const [query, setQuery] = useState('');
-	const [searchParams, setSearchParams] = useSearchParams();
-	const movieSearchParams = searchParams.get('query');
+  const [films, setFilms] = useState([]);
+  const [query, setQuery] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const movieSearchParams = searchParams.get("query");
 
-	const handleChange = e => {
-		const {value} = e.target;
-		setQuery(value.toLowerCase());
-	}
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setQuery(value.toLowerCase());
+  };
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		setSearchParams({query: query});
-		setQuery('');
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchParams({ query: query });
+    setQuery("");
+  };
 
-	useEffect(() => {
-		if (!movieSearchParams) {
-			return;
-		}
+  useEffect(() => {
+    if (!movieSearchParams) {
+      return;
+    }
 
-		const getMovie = async () => {
-			try {
-				const data = await searchMovies(movieSearchParams)
-				if (!data.length) {
-					alert('Something went wrong');
-					return;
-				}
-				setFilms(data);
-			}
-			catch (err) {
-				console.log(err)
-			}
-		}
+    const getMovie = async () => {
+      try {
+        const data = await searchMovies(movieSearchParams);
+        if (!data.length) {
+          alert("Something went wrong");
+          return;
+        }
+        setFilms(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-		getMovie()
-	}, [movieSearchParams]);
+    getMovie();
+  }, [movieSearchParams]);
 
-	return (
-		<>
-			<form className={s.form} onSubmit={handleSubmit}>
-				<input
-					className={s.input}
-					onChange={handleChange}
-					type="text"
-					value={query}
-					placeholder="Enter a name of film"
-				/>
-				<button className={s.button} type="submit">Search</button>
-			</form>
+  return (
+    <>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <input
+          className={s.input}
+          onChange={handleChange}
+          type="text"
+          value={query}
+          placeholder="Enter a name of film"
+        />
+        <button className={s.button} type="submit">
+          Search
+        </button>
+      </form>
 
-			{films && <MoviesList movies={films}/>}
-		</>
-	)
-}
+      {films.length > 0 && <MoviesList movies={films} />}
+    </>
+  );
+};
 
 export default Movies;
