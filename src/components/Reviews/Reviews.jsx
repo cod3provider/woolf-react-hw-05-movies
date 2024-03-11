1048fc77f7060d3253dfc582c6aaed3ef615f18e
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-11 08:53:25 +0100

    refactored Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,40 +1,38 @@
-import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import {getReviews} from "../../api/api.js";
+import { getReviews } from "../../api/api.js";
 
-import s from './Reviews.module.css';
+import s from "./Reviews.module.css";
 
 const Reviews = () => {
-	const [review, setReview] = useState([]);
-	const {movieId} = useParams();
+  const [review, setReview] = useState([]);
+  const { movieId } = useParams();
 
-	useEffect(() => {
-		const getMovieReviews = async () => {
-			try {
-				const dataReview = await getReviews(movieId);
-				setReview(dataReview);
-			}
-			catch (err) {
-				console.log(err);
-			}
-		}
+  useEffect(() => {
+    const getMovieReviews = async () => {
+      try {
+        const dataReview = await getReviews(movieId);
+        setReview(dataReview);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-		getMovieReviews();
-	}, [movieId]);
+    getMovieReviews();
+  }, [movieId]);
 
-	const reviewItem = review.map(({ id ,author, content }) => (
-		<li className={s.item} key={id}>
-			<p className={s.text}>{content}</p>
-			<p className={s.author}>{author}</p>
-		</li>
-	))
-
-	return (
-		<ul className={s.list}>
-			{reviewItem}
-		</ul>
-	)
-}
+  return (
+    <ul className={s.list}>
+      {review.length > 0 &&
+        review.map(({ id, author, content }) => (
+          <li className={s.item} key={id}>
+            <p className={s.text}>{content}</p>
+            <p className={s.author}>{author}</p>
+          </li>
+        ))}
+    </ul>
+  );
+};
 
 export default Reviews;
