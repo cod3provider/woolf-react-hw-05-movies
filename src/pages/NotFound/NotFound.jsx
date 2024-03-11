0142d49c61fa57b7e35206c691be57ff0ca96da5
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-11 09:17:40 +0100

    added to NotFound page: link to main page, styles

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import s from "./NotFound.module.css";
+
 const NotFound = () => {
-  return <p>Page Not Found</p>;
+  return (
+    <div className={s.wrap}>
+      <h2 className={s.title}>Page Not Found</h2>
+      <Link className={s.link} to="/">
+        Go to home page
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
