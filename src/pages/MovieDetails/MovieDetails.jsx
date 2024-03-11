2b3f28c8091e0c0868ee8c6a90cac3dd22731d00
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-11 08:53:54 +0100

    added Go Back button

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,58 +1,66 @@
-import {Suspense, useEffect, useState} from "react";
-import { NavLink, Outlet, useLocation, useParams} from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+import {
+  Link,
+  NavLink,
+  Outlet,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 
 import AboutMovie from "../../components/AboutMovie/AboutMovie.jsx";
 
-import {getDetails} from "../../api/api.js";
+import { getDetails } from "../../api/api.js";
 
 import s from "./MovieDetails.module.css";
 
 const MovieDetails = () => {
-	const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState(null);
 
-	const {movieId} = useParams();
-	const location = useLocation();
+  const { movieId } = useParams();
+  const location = useLocation();
 
-	useEffect(() => {
-		const getMovieDetails = async () => {
-			try {
-				const film = await getDetails(movieId);
-				setMovie(film);
-			}
-			catch (err) {
-				console.log(err);
-			}
-		}
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      try {
+        const film = await getDetails(movieId);
+        setMovie(film);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-		getMovieDetails();
-	}, [movieId]);
+    getMovieDetails();
+  }, [movieId]);
 
-	// console.log(movie)
+  const from = location.state?.from ?? "/";
 
-	const from = location.state?.from ?? '/';
+  return (
+    <>
+      {movie && (
+        <div className={s.infoWrap}>
+          <Link className={s.goBack} to={from}>Go back</Link>
+          <AboutMovie about={movie} />
+          <h2>Info</h2>
+          <ul className={s.list}>
+            <li>
+              <NavLink className={s.linksList} to="cast" state={{ from }}>
+                Cast
+              </NavLink>
+            </li>
 
-	return (
-		<>
-			{movie && (
-				<div className={s.infoWrap}>
-					<AboutMovie about={movie}/>
-					<h2>Info</h2>
-					<ul className={s.list}>
-						<li>
-							<NavLink className={s.linksList} to='cast' state={{from}}>Cast</NavLink>
-						</li>
-
-						<li>
-							<NavLink className={s.linksList} to='reviews' state={{from}}>Review</NavLink>
-						</li>
-					</ul>
-					<Suspense fallback={<p>...Loading</p>}>
-						<Outlet/>
-					</Suspense>
-				</div>
-			)}
-		</>
-	)
-}
+            <li>
+              <NavLink className={s.linksList} to="reviews" state={{ from }}>
+                Review
+              </NavLink>
+            </li>
+          </ul>
+          <Suspense fallback={<p>...Loading</p>}>
+            <Outlet />
+          </Suspense>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default MovieDetails;
